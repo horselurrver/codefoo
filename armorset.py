f79f8a5c99827eabfb3746aab232fef242ed2647
gwcadmin <gwcadmin@gwcadmins-MacBook-Pro.local>
2019-07-09 12:07:26 -0400

    Add some helper functions

--- armorset.py
+++ armorset.py
@@ -1,5 +1,28 @@
 import csv
 
+# sort array based on value, with item in index 0 having the biggest value
+def sortList(array):
+    sortedArray = sorted(array, key=lambda x: x.value, reverse=True)
+    return sortedArray
+
+# get how many crowns we'll be spending on the current shopping cart
+def getTotalCost(shoppingCart):
+    total = 0
+    for item in shoppingCart:
+        total += int(item.cost)
+    return total
+
+# get the nth max object from array (check the value)
+# if n = 0, get the object with the biggest value
+def getNthMax(n, array):
+    sorted = sortList(array)
+    try:
+        result = sorted[n]
+        return result
+    except IndexError:
+        print('Improper index!')
+
+# load inventory from json file into a list
 def loadInventory():
     helmet = []
     boots = []
@@ -30,25 +53,30 @@ def loadInventory():
             line_count += 1
     return [helmet, boots, leggings, chest]
 
-# Define class item for each item in the inventory
+# define class item for each item in the inventory
 class Item:
     def __init__(self, type, name, cost, value):
         self.type = type
         self.name = name
         self.cost = cost
         self.value = value
-    def getType(self):
-        return self.type
-    def getName(self):
-        return self.name
-    def getCost(self):
-        return self.cost
-    def getValue(self):
-        return self.value
     def __repr__(self):
-        return '[{}, {}, {}, {}]'.format(self.type, self.name, self.cost, self.value)
+        return '[{}, {}, Cost: {}, Value: {}]'.format(self.type, self.name, self.cost, self.value)
 
 budget = 300 # crowns available to purchase equipment
-
+myShoppingCart = []
+# load separate inventories for the different equipment
 helmet, boots, leggings, chest = loadInventory()
-print(max(helm.value for helm in helmet))
+inventory = helmet + boots + leggings + chest
+# make an inventory with all items that are sorted by value
+inventory = sortList(inventory)
+
+for category in [helmet, boots, leggings, chest]:
+    maximum = getNthMax(0, category)
+    myShoppingCart.append(maximum)
+# add one more item to shopping cart
+
+
+cost = getTotalCost(myShoppingCart)
+print('My shopping cart: ' + str(myShoppingCart))
+print('Cost: ' + str(cost))
