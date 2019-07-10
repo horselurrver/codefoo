b7aa7bc1a7f15ba77b66812188cca63b62f233db
gwcadmin <gwcadmin@gwcadmins-MacBook-Pro.local>
2019-07-10 15:16:41 -0400

    Finish armor set

--- armorset.py
+++ armorset.py
@@ -1,32 +1,41 @@
 import csv
 
+"""
+Algorithm for optimizing:
+while True:
+    Get maximum of each category plus next highest value not in shopping cart
+    If over budget,
+        Get the next highest value in the inventory
+        Check the category and replace the item in the shopping cart with same category
+    If there are two items of the same category in the shopping cart, replace the more expensive item
+    else
+            break!
+"""
 # takes shopping cart and new item, then replaces item of same category in the inventory
 # if two items of same category, replace the more expensive item
 # returns a new inventory
 def replaceItem(shoppingCart, newItem):
-    newInventory = inventory
-    if hasTwoOfCategory(inventory, newItem.type):
-        print('Two of category')
+    newCart = shoppingCart
+    # mark newItem in inventory --> has been in shopping cart
+    if hasTwoOfCategory(newCart, newItem.type):
         # replace the more expensive item
         # by first finding indices of items with the same category as the new item
         itemIndicesWithSameCategory = []
-        for index, value in enumerate(inventory):
+        for index, value in enumerate(newCart):
             if value.type == newItem.type:
                 itemIndicesWithSameCategory.append(index)
         index1 = itemIndicesWithSameCategory[0]
         index2 = itemIndicesWithSameCategory[1]
         # now replace the more expensive item
-        if inventory[index1].cost > inventory[index2].cost:
-            inventory[index1] = newItem
+        if newCart[index1].cost > newCart[index2].cost:
+            newCart[index1] = newItem
         else:
-            inventory[index2] = newItem
+            newCart[index2] = newItem
     else:
-        print('Only one of category')
-        for index, value in enumerate(newInventory):
+        for index, value in enumerate(newCart):
             if value.type == newItem.type:
-                newInventory[index] = newItem
-    print('New inventory: ' + str(newInventory))
-    return newInventory
+                newCart[index] = newItem
+    return newCart
 
 # checks if inventory has two of same category items
 def hasTwoOfCategory(inventory, category):
@@ -36,11 +45,16 @@ def hasTwoOfCategory(inventory, category):
             result.append(item)
     return len(result) == 2
 
+
 # given sorted inventory, return next highest value equipment not in shopping cart
-def getNextBest(inventory, shoppingCart):
-    for i in range(len(inventory)):
-        if inventory[i] not in shoppingCart:
-            return inventory[i]
+def getNext(shoppingCart):
+    for item in getNextBestInventory:
+        if item in shoppingCart:
+            getNextBestInventory.remove(item)
+        else:
+            result = item
+            getNextBestInventory.remove(item)
+            return result
 
 # sort array based on value, with item in index 0 having the biggest value
 def sortList(array):
@@ -49,13 +63,16 @@ def sortList(array):
 
 # get how many crowns we'll be spending on the current shopping cart
 def getTotalCost(shoppingCart):
-    print('Shopping cart: ' + str(shoppingCart))
     total = 0
     for item in shoppingCart:
-        print('Item cost: ' + str(item.cost))
         total += int(item.cost)
     return total
 
+def getValue(shoppingCart):
+    total = 0
+    for item in shoppingCart:
+        total += int(item.value)
+    return total
 # get the nth max object from array (check the value)
 # if n = 0, get the object with the biggest value
 def getNthMax(n, array):
@@ -77,14 +94,12 @@ def loadInventory():
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
-                # print(' '.join(row))
                 pass
             else:
                 type = row[0]
                 name = row[1]
                 cost = row[2]
-                value = row[3]
-                # print(f'{type}, {name}, {cost}, {value}')
+                value = int(row[3])
                 item = Item(type, name, cost, value)
                 if type == 'Helmet':
                     helmet.append(item)
@@ -107,43 +122,38 @@ class Item:
     def __repr__(self):
         return '[{}, {}, Cost: {}, Value: {}]'.format(self.type, self.name, self.cost, self.value)
 
-budget = 200 # crowns available to purchase equipment
+budget = 300 # crowns available to purchase equipment
 myShoppingCart = []
 # load separate inventories for the different equipment
 helmet, boots, leggings, chest = loadInventory()
 inventory = helmet + boots + leggings + chest
-# make an inventory with all items that are sorted by value
-inventory = sortList(inventory)
+# make an inventory with all items that are sorted by value (descending)
+inventory.sort(key=lambda x: x.value)
+inventory.reverse()
+getNextBestInventory = inventory.copy()
 
-"""
-Algorithm for optimizing:
-while True:
-    Get maximum of each category plus next highest value not in shopping cart
-    If over budget,
-        Get the next highest value in the inventory
-        Check the category and replace the item in the shopping cart with same category
-    If there are two items of the same category in the shopping cart, replace the more expensive item
-    else
-            break!
-"""
 counter = 0
 for category in [helmet, boots, leggings, chest]:
     maximum = getNthMax(counter, category)
     myShoppingCart.append(maximum)
     #    add one more item to shopping cart
-myShoppingCart.append(getNextBest(inventory, myShoppingCart))
-print('My shopping cart: ' + str(myShoppingCart))
+myShoppingCart.append(getNext(myShoppingCart))
+
+finalShoppingCart = myShoppingCart.copy()
 while True:
-    print('Total cost: ' + str(getTotalCost(myShoppingCart)))
+    if len(getNextBestInventory) == 0:
+        break
     if getTotalCost(myShoppingCart) > budget:
         # get item with next best value
-        nextItem = getNextBest(inventory, myShoppingCart)
-        print('The next best item: ' + str(nextItem))
+        nextItem = getNext(myShoppingCart)
         # replace the item with the same category by next best valued item
         myShoppingCart = replaceItem(myShoppingCart, nextItem)
+        finalShoppingCart = myShoppingCart.copy()
     else:
         break
 
-cost = getTotalCost(myShoppingCart)
-print('My shopping cart: ' + str(myShoppingCart))
+print(str(finalShoppingCart))
+cost = getTotalCost(finalShoppingCart)
+value = getValue(finalShoppingCart)
 print('Cost: ' + str(cost))
+print('Value: ' + str(value))
